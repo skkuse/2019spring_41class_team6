345bcba57925408bcae799fe2dd971d37f47138f
seventeen <powdragon1@gmail.com>
2019-06-10 00:01:12 +0900

    add Uploading component

---
+++ src/components/Uploading.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="uploading">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+#uploading {
+  position: absolute;
+  z-index: 2000;
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  background: black;
+  opacity: 0.7;
+}
+</style>
