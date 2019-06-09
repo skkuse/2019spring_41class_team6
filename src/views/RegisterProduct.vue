796d82ee56855f7b8b915d66c4a2c3dc2e565746
seventeen <powdragon1@gmail.com>
2019-06-10 00:02:14 +0900

    apply new schema of products (RegisterProduct)

--- src/views/RegisterProduct.vue
+++ src/views/RegisterProduct.vue
@@ -1,10 +1,40 @@
 <template>
   <div>
+    <Uploading style="display: none"></Uploading>
     <div style="text-align: center">
       <form id="myForm" @submit.prevent="uploadData">
-        <input type="text" name="title" v-model="title" /><br />
-        <input type="file" name="image" @change="getFile" /><br />
-        <textarea name="body" v-model="body"></textarea><br />
+        <input
+          type="text"
+          name="title"
+          v-model="title"
+          placeholder="상품 이름"
+        /><br />
+        <input type="file" name="thumbnail" @change="getFile" /><br />
+        <input
+          type="text"
+          name="thumbnailDescription"
+          v-model="thumbnailDescription"
+          placeholder="썸네일에 대한 설명"
+        /><br />
+        <input type="file" name="mainImages" @change="getFile" multiple /><br />
+        <textarea
+          name="mainDescription"
+          v-model="mainDescription"
+          placeholder="상품의 상세 설명"
+        ></textarea
+        ><br />
+        <input
+          type="file"
+          name="detailImages"
+          @change="getFile"
+          multiple
+        /><br />
+        <input
+          type="number"
+          name="price"
+          v-model="price"
+          placeholder="상품가격"
+        /><br />
         <button type="submit">submit</button>
       </form>
     </div>
@@ -13,49 +43,109 @@
 
 <script>
 import { db, storage } from "@/firebase";
+import Uploading from "@/components/Uploading.vue";
+let images = {
+  thumbnail: [],
+  mainImages: [],
+  detailImages: []
+};
 
-let file = null;
+let urls = {
+  thumbnail: [],
+  mainImages: [],
+  detailImages: []
+};
+
+function toggleProgress() {
+  document.getElementById("uploading").style.display =
+    document.getElementById("uploading").style.display === "none"
+      ? "block"
+      : "none";
+}
+
+function uploadImages(images) {
+  return new Promise((resolve, reject) => {
+    let cnt = 0;
+    let dataLength = 0;
+    for (let key in images) {
+      dataLength += images[key].length;
+    }
+    for (let key in images) {
+      for (let i = 0; i < images[key].length; i++) {
+        storage
+          .ref()
+          .child("productImages/" + images[key][i].name)
+          .put(images[key][i])
+          .then(snapshot => {
+            snapshot.ref
+              .getDownloadURL()
+              .then(url => {
+                urls[key][i] = url;
+                if (++cnt === dataLength) {
+                  resolve("success");
+                }
+              })
+              .catch(err => {
+                reject(err);
+              });
+          })
+          .catch(err => {
+            reject(err);
+          });
+      }
+    }
+  });
+}
 
 export default {
   name: "register-product",
+  components: {
+    Uploading
+  },
   data: () => {
     return {
-      message: "",
       title: null,
-      body: null
+      thumbnailDescription: null,
+      mainDescription: null,
+      price: null,
+      showUploading: false
     };
   },
   methods: {
     uploadData: function() {
-      storage
-        .ref()
-        .child("productImages/" + file.name)
-        .put(file)
-        .then(snapshot => {
-          snapshot.ref
-            .getDownloadURL()
-            .then(url => {
-              db.collection("products")
-                .add({
-                  title: this.title,
-                  body: this.body,
-                  imageUrl: url,
-                  createdAt: +new Date()
-                })
-                .then(() => {
-                  alert("업로드 완료");
-                });
+      toggleProgress();
+      uploadImages(images)
+        .then(() => {
+          db.collection("products")
+            .add({
+              createdAt: +new Date(),
+              title: this.title,
+              thumbnail: urls.thumbnail[0],
+              thumbnail_description: this.thumbnailDescription,
+              main_description: this.mainDescription,
+              main_images: urls.mainImages,
+              detail_images: urls.detailImages,
+              price: this.price
+            })
+            .then(() => {
+              toggleProgress();
+              alert("업로드 완료");
+              location.reload();
             })
             .catch(error => {
-              alert(error);
+              alert("db upload fail:" + error);
             });
         })
-        .catch(error => {
-          alert(error);
+        .catch(err => {
+          console.log(err);
         });
     },
     getFile: function(e) {
-      file = e.target.files[0];
+      const fieldName = e.target.name;
+      images[fieldName] = [];
+      for (let i = 0; i < e.target.files.length; i++) {
+        images[fieldName].push(e.target.files[i]);
+      }
     }
   }
 };
