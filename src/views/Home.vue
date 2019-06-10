221ecbe6c15193c94c750a554b5483b507f48efa
Ssioo <41153640+Ssioo@users.noreply.github.com>
2019-06-10 16:56:56 +0900

    Home Text Description Changed
    .

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,14 +10,13 @@
           class="row align-items-start align-items-md-center justify-content-end"
         >
           <div class="col-md-5 text-center text-md-left pt-5 pt-md-0">
-            <h1 class="mb-2">Finding Your Perfect Shoes</h1>
+            <h1 class="mb-2">Finding Your Identity</h1>
             <div class="intro-text text-center text-md-left">
               <p class="mb-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus at iaculis quam. Integer accumsan tincidunt fringilla.
+                No one's normal with us. Show Your Identitiy to others.
               </p>
               <p>
-                <a href="#" class="btn btn-sm btn-primary">Shop Now</a>
+                <a href="#" class="btn btn-sm btn-primary">Sign Up</a>
               </p>
             </div>
           </div>
