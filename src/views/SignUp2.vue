b835d8ef5cc30d559587822ff646ab035f32d7fb
Ssioo <41153640+Ssioo@users.noreply.github.com>
2019-06-11 15:11:27 +0900

    Create SignUp2.vue

---
+++ src/views/SignUp2.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="sign-up">
+        <p>Let's create a new accuont!</p>
+        <input type="text",  v-model="email" placeholder="Email"></input>
+        <br>
+        <input type="password" v-model="password" placeholder="Password"></input>
+        <br>
+        <button v-on:click="signUp2">Sign Up</button>
+        <span>or go back to <router-link to="/SignIn">Sign in</router-link>. </span> <!-- ?? -->
+    </div>
+</template>
+
+<script>
+    import firebase from 'firebase'
+
+    export default {
+        name: "signUp2",
+        data: function () {
+            return {
+                email: '',
+                password: ''
+            }
+        },
+        methods: {
+            signUp2: function () {
+                firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
+                    function (user) {
+                        alert('Your account has been created!')
+                    },
+                    function (err) {
+                        alert('Oops. '+ err.message)
+                    }
+                );
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
